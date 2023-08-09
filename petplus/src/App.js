f5ec09b49fea20e39621771216b954f3d909be2f
Gihan Shirantha <shirantha1999@gmail.com>
2023-08-09 22:16:57 +0530

    AOS install and implementation

--- petplus/src/App.js
+++ petplus/src/App.js
@@ -2,7 +2,15 @@
 import './App.css';
 import Home from './Page/Home'
 
+import { useEffect } from "react";
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 function App() {
+  useEffect(()=>{
+    Aos.init();
+  },[]);
+   
   return (
     <div >
       <Home/>
--- petplus/src/Components/Hero/Hero.jsx
+++ petplus/src/Components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <Row>
                 <Col lg='12'>
                     <div className="hero_title">
-                        <h1><span>Caring</span> for Pets, <span>Creating</span> Smiles</h1>
+                        <h1 data-aos="fade-up" data-aos-duration="2000"><span>Caring</span> for Pets, <span>Creating</span> Smiles</h1>
                         <p>PetPlus Pet Care Center is a comprehensive facility dedicated to providing exceptional care for pets. Our center offers a range of services to ensure the health, happiness, and well-being of furry companions. With a team of highly skilled and compassionate professionals, we strive to create a comfortable and safe environment for pets and their owners.</p>
                     </div>
                     <div className="hero_buttons">
